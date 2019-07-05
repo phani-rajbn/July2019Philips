474efef641d017eee3220fe8db35840641680cf6
Phaniraj B.N <35284581+phani-rajbn@users.noreply.github.com>
2019-07-05 15:54:32 +0530

    Add files via upload

---
+++ PhilipsDotnetTraining/SampleConApp/ReflectionExample.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Reflection;
+
+/*
+ * With Reflection, we can read the info about the class and its methods by accessing the metadata of the Assembly that we programmatically load....
+ * Assembly contains Classes, Interfaces, structs, Enums, Delegates grouped into namespaces. 
+ * 
+ */
+namespace SampleConApp
+{
+    class ReflectionExample
+    {
+        static Assembly assembly;
+        static Type selectedType;
+        static MethodInfo selectedMethod;
+        const string dllFile = @"C:\Users\phani\source\repos\PhilipsDotnetTraining\MathComponentLib\bin\Debug\MathComponentLib.dll";
+
+        static void getClassDetails()
+        {
+            assembly = Assembly.LoadFile(dllFile);
+            if (assembly == null)
+                return;
+            Type[] ourClasses = assembly.GetTypes();
+            foreach (var type in ourClasses)
+                Console.WriteLine(type.FullName);
+        }
+        static void Main(string[] args)
+        {
+            getClassDetails();
+            getMethodDetails();
+            getParameterDetails();
+            object instance = Activator.CreateInstance(selectedType);
+            if(selectedMethod.GetParameters().Length == 0)
+            {
+                //No parameters for this function...
+                var value = selectedMethod.Invoke(instance, null);
+                Console.WriteLine(value);
+                return;
+            }
+            else
+            {
+                var parameters = selectedMethod.GetParameters();
+                object[] pmValues = new object[parameters.Length];
+                for (int i = 0; i < parameters.Length; i++)
+                {
+                    Console.WriteLine("Enter the value for the parameter {0} of the data type {1}", parameters[i].Name, parameters[i].ParameterType.Name);
+                    pmValues[i] = Convert.ChangeType(Console.ReadLine(), parameters[i].ParameterType);
+                }
+                Console.WriteLine("All the parameters are set, \nLets invoke");
+                var result = selectedMethod.Invoke(instance, pmValues);
+                Console.WriteLine("The Result: " + result);
+            }
+        }
+
+        private static void getParameterDetails()
+        {
+            
+            Console.WriteLine("Enter the method to invoke from the list above");
+            selectedMethod = selectedType.GetMethod(Console.ReadLine());
+            if (selectedMethod == null)
+            {
+                Console.WriteLine("This method is not supported by our class");
+            }
+        }
+
+        private static void getMethodDetails()
+        {
+
+            Console.WriteLine("Please enter the class that U want to find details");
+            var name = Console.ReadLine();
+            selectedType = assembly.GetType(name, false, true);
+            if (selectedType == null)
+            {
+                Console.WriteLine("Invalid class selected");
+                return;
+            }
+
+            var methods = selectedType.GetMethods();
+            foreach (var method in methods)
+            {
+                Console.WriteLine($"{method.Name}, Its returntype: {method.ReturnType.Name}, Its Arguments: ");
+                foreach (var pm in method.GetParameters())
+                    Console.WriteLine("Parameter {0}, DataType:{1}", pm.Name, pm.ParameterType.Name);
+            }
+        }
+    }
+}
---
+++ PhilipsDotnetTraining/SampleConApp/UsingDLL.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+/*If a class of a DLL has to be consumed in this project, then U should do the following things:
+ * Add the reference of the DLL: Project Reference(If its in the same soln) or Browse(If the Project is not in the current soln).
+ * Use the namespace in which the class is created...By Default, the projectname is the namespace of the project...
+ * Create the object of the class like any other class creation U have done till now...
+ * Dlls make UR App more modular in nature as the code is divided into multiple projects and code is reusable across different projects. 
+ * Dynamic Link Library files are loaded at runtime when the App begins its execution. 
+ * Sometimes, U may have to load the dll programmatically without referencing it or using the namespaces or sometimes load them when U need instead of loaded at the begining of UR project execution. 
+ * This is done thro REFLECTION. Reflection is a library of classes that allows to read the metadata of the Assembly and get its Information like Types, Methods and so forth and with which we could load the classes and invoke the methods of those classes. 
+ * 
+*/
+namespace SampleConApp
+{
+    using MathComponentLib;
+    class UsingDLL
+    {
+        static void Main(string[] args)
+        {
+            var com = new MathObject();
+            var data = com.AddFunc(123, 234);
+            Console.WriteLine(data);
+        }
+    }
+}
